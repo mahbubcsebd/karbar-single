0b49eb18857e9e9be6e15bf2756929383d7b69c9
Mahbubur Rahman <mahbub.cse.me@gmail.com>
2025-05-05 16:34:21 +0600

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-// const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,87 +5,18 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
     minimumCacheTTL: 60,
-    // unoptimized: true,
-    // domains: [
-    //     'admin.daarib.com',
-    //     'shop.uibarn.com',
-    //     '127.0.0.1:8000',
-    //     '192.168.0.124',
-    //     '127.0.0.1',
-    //     'admin.evascollection.shop',
-    //     'img.freepik.com',
-    // ],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'admin.daarib.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.bioschale.de',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.evascollection.shop',
-      },
       {
         protocol: 'https',
         hostname: 'demoadmin.karbar.shop',
       },
       {
         protocol: 'https',
-        hostname: 'admin.furnitobd.com',
-      },
-      {
-        protocol: 'https',
         hostname:
           new URL(process.env.NEXT_PUBLIC_API_BASE_URL).hostname || 'localhost',
       },
-      {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.dreamshopkeeper.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.dhakaibd.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'karbar.s3.ap-southeast-2.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '127.0.0.1:8000',
-      },
-      {
-        protocol: 'http',
-        hostname: '192.168.0.149',
-      },
-      {
-        protocol: 'https',
-        hostname: '127.0.0.1',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.facebook.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.dhakaibd.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'admin.romartbd.com',
-      },
     ],
   },
-  // experimental: {
-  //     missingSuspenseWithCSRBailout: false,
-  // },
 };
 
 export default nextConfig;
